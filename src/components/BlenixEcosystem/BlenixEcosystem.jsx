34742364900cf3c2cbe18fb7330be22c4e7d6c1b
Abdur Rehman <abdurrehmansaifi784@gmail.com>
2025-01-27 02:20:38 +0500

    feat: enhance GSAP animations in BlenixEcosystem component for title and paragraph elements

--- src/components/BlenixEcosystem/BlenixEcosystem.jsx
+++ src/components/BlenixEcosystem/BlenixEcosystem.jsx
@@ -8,7 +8,6 @@ import sections from './data';
 const BlenixEcosystem = () => {
 
     const [activeIndex, setActiveIndex] = useState(0);
-    const sectionRef = useRef(null);
     useEffect(() => {
         const interval = setInterval(() => {
             setActiveIndex(prevIndex => (prevIndex + 1) % sections.length);
@@ -16,11 +15,26 @@ const BlenixEcosystem = () => {
         return () => clearInterval(interval);
     }, []);
     const cutOffPoint = Math.round((activeIndex / (sections.length - 1)) * 100);
+    const sectionRef = useRef(null);
+    const titleRef = useRef(null);
+    const paragraphRef = useRef(null);
     useEffect(() => {
         gsap.fromTo(
             sectionRef.current,
-            { opacity: 0, y: -20 }, // initial state
+            { opacity: 0, y: -20 }, // initial state for the image
             { opacity: 1, y: 0, duration: 0.5, ease: 'power2.in' }
+        );
+
+        gsap.fromTo(
+            titleRef.current,
+            { opacity: 0, x: -20 }, // initial state for the heading
+            { opacity: 1, x: 0, duration: 0.5, ease: 'power2.in' } // Added delay to stagger animation
+        );
+
+        gsap.fromTo(
+            paragraphRef.current,
+            { opacity: 0, x: -20 }, // initial state for the paragraph
+            { opacity: 1, x: 0, duration: 0.5, ease: 'power2.in' } // Added delay to stagger animation
         );
     }, [activeIndex]);
     return (
@@ -58,11 +72,11 @@ const BlenixEcosystem = () => {
                         ))}
                     </div>
                 </div>
-                <div className="w-full md:w-[55%]">
+                <div className="w-full md:w-[55%] ">
                     {sections.map((section, index) => (
                         <div
                             key={index}
-                            className={`w-full rounded-[40px] p-5 bg_glass transition-opacity overflow-hidden duration-500 ease-in-out ${activeIndex === index ? 'block' : 'hidden'}`}
+                            className={`w-full rounded-[40px] p-5 bg_glass overflow-hidden min-h-[90vh] lg:min-h-[80vh] xl:min-h-[65vh] flex flex-col justify-evenly ${activeIndex === index ? 'block ' : 'hidden'}`}
                         >
                             <div className="flex justify-center">
                                 <Image
@@ -74,10 +88,12 @@ const BlenixEcosystem = () => {
                                     ref={activeIndex === index ? sectionRef : null}
                                 />
                             </div>
-                            <h5 className="lg:text-xl text-white my-2">{section.title}</h5>
-                            <p className="text-white font-light">
-                                {section.description}
-                            </p>
+                            <div className="">
+                                <h5 className="lg:text-xl text-white my-2" ref={activeIndex === index ? titleRef : null}>{section.title}</h5>
+                                <p className="text-white font-light" ref={activeIndex === index ? paragraphRef : null}>
+                                    {section.description}
+                                </p>
+                            </div>
                         </div>
                     ))}
                 </div>
